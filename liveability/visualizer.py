39959b24d47168367b989a25769fac3807a3100b
shuiguodao111 <shuiguodao111@gmail.com>
2026-03-30 01:37:16 +0800

    优化了布局并加入雷达图）

--- liveability/visualizer.py
+++ liveability/visualizer.py
@@ -169,7 +169,7 @@ def make_gauge(city_name, score):
             'font': {'size': 24, 'color': title_color}
         },
         number={
-            'font': {'size': 48, 'color': bar_color},
+            'font': {'size': 18, 'color': bar_color},
             'suffix': " pts"
         },
         gauge={
